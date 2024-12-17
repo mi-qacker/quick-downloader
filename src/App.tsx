5dab8932088e52ef9b9ee63b291b12b03e13135a
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2024-12-17 22:13:14 +0300

    Prettier config options

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>Main page</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>Main page</h1>
+        </>
+    );
 }
 
 export default App;
